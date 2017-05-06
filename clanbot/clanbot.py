ef5e3cd3efaa5e572e3705383df1a07187301b52
Isaac K <ethernic@voidwall.com>
2017-05-06 00:38:32 -0700

    more updates

--- clanbot/clanbot.py
+++ clanbot/clanbot.py
@@ -1,8 +1,10 @@
-from dateutil.parser import parse
+from dateutil import parser
+from datetime import datetime
 from pytz import timezone
 import os
 import json
 import requests
+import pytz
 
 class ClanBot(object):
     """
@@ -10,15 +12,22 @@ class ClanBot(object):
     """
 
     def __init__(self):
-        self.api_key = str(os.environ["BUNGIE_API_KEY"])
+        self.api_key = str(os.environ['BUNGIE_API_KEY'])
         self.session = requests.Session()
-        self.session.headers["X-API-Key"] = self.api_key
-        
-        self.user_base_uri = "https://www.bungie.net/Platform/User/"
-        self.destiny_base_uri = "https://www.bungie.net/Platform/Destiny/"
-        self.GAMERTAG = "Ethernic"
+        self.session.headers['X-API-Key'] = self.api_key  
+        self.user_base_uri = 'https://www.bungie.net/Platform/User/'
+        self.destiny_base_uri = 'https://www.bungie.net/Platform/Destiny/'
+        self.group_base_uri = 'https://www.bungie.net/Platform/Group/'
+        self.clan_dict = {'Members':{}}
+        self.clan_id = '548440' #http://bungienetplatform.wikia.com/wiki/GroupSearch
+        self.clan_members = []
+        self.est = timezone('US/Eastern')
+        self.GAMERTAG = 'Ethernic'
         self.gamertag = None
         self.last_played = None
+        self.last_played_days = None
+        self.member_since = None
+        self.member_since_days = None
         self.membership_id = None 
         self.membership_type = '2' # -1=both, 1=xbox, 2=psn
         self.destiny_membership_id = None
@@ -26,46 +35,90 @@ class ClanBot(object):
         self.timefmt = '%Y-%m-%d %H:%M:%S %Z'
         
     def display_stats(self):
-        print "{0}".format(self.gamertag)
-        print "Last played: {0}".format(self.last_played.strftime(self.timefmt)) 
+        print '{0}'.format(self.gamertag)
+        if not self.clan_dict['Members']:
+            play_date = parser.parse(self.last_played).astimezone(self.est).strftime(self.timefmt)
+            print 'Last played: {0} ({1} days)'.format(play_date, self.last_played_days)
+        else:
+            play_date = parser.parse(self.last_played).astimezone(self.est).strftime(self.timefmt)
+            self.member_since = self.clan_dict['Members'][self.gamertag]['approvalDate']
+            member_date = parser.parse(self.member_since).astimezone(self.est).strftime(self.timefmt)
+            self.member_since_days = str(self.date_math(parser.parse(self.member_since)))
+            print 'Member since: {0} ({1} days)'.format(member_date, self.member_since_days)
+           # print 'Last played: {0}'.format(play_date.strftime(self.timefmt)) 
     
-    def get_destiny_membership_id(self):
-        full_uri = self.user_base_uri + 'GetBungieAccount/' + self.membership_id + '/254/'
+    def get_clan_id(self):
         i = 0
-        parsed = self.get_request(full_uri)
-        while i < len(parsed['Response']['destinyAccounts']):
-            if parsed['Response']['destinyAccounts'][i]['userInfo']['membershipType'] == int(self.membership_type):
-                self.destiny_membership_id = parsed['Response']['destinyAccounts'][i]['userInfo']['membershipId']
-                break
-            else:
-                i += 1
-        #self.destiny_membership_id = parsed['Response']['destinyAccounts'][0]['userInfo']['membershipId']
-    
-    def get_destiny_details(self):
-        full_uri = self.destiny_base_uri + 'SearchDestinyPlayer/-1/' + self.gamertag
+        full_uri = self.user_base_uri + 'GetBungieAccount/' + self.destiny_membership_id + '/' + self.membership_type + '/'
+        details = self.get_request(full_uri)
+        if len(details['Response']['clans']) > 1:
+            while i < len(details['Response']['clans']):
+                if details['Response']['clans'][i]['platformType'] == 2:
+                    self.clan_id = str(details['Response']['clans'][i]['groupId'])
+                    break
+                else:
+                    i += 1
+        else:
+            self.clan_id = str(details['Response']['clans'][i]['groupId'])
+
+    def get_clan_members(self):
+        current_page = 1
+        has_more = True
+        while has_more:
+            full_uri = self.group_base_uri + self.clan_id + '/MembersV3/?currentPage='
+            paged_members = self.get_request(full_uri+str(current_page))
+            self.clan_members.extend(paged_members['Response']['results'])
+            current_page += 1
+            has_more = paged_members['Response']['hasMore']
+        i = 0
+        while i < len(self.clan_members):
+            GAMERTAG = str(self.clan_members[i]['user']['psnDisplayName'])
+            try:
+                self.clan_dict['Members'][GAMERTAG]['approvalDate'] = self.clan_members[i]['approvalDate']
+            except KeyError:
+                self.clan_dict['Members'][GAMERTAG] = {'approvalDate': self.clan_members[i]['approvalDate']}
+            self.clan_dict['Members'][GAMERTAG]['membershipId'] = self.clan_members[i]['membershipId']
+            i += 1
+
+    def date_math(self, older):
+        now = datetime.now(pytz.utc)
+        diff = now - older
+        return diff.days
+
+    def get_destiny_details(self, *GAMERTAG):
+        i = 0
+        if not GAMERTAG:
+            PLAYER = self.gamertag
+        else:
+            PLAYER = GAMERTAG[0]
+        full_uri = self.destiny_base_uri + 'SearchDestinyPlayer/-1/' + PLAYER
         details = self.get_request(full_uri)
         if len(details['Response']) > 1:
-            i = 0
             while i < len(details['Response']):
-                if details['Response'][i]['membershipType'] == 2:
+                if details['Response'][i]['membershipType'] == int(self.membership_type):
                     self.destiny_membership_id = details['Response'][i]['membershipId']
                     break
                 else:
                     i += 1
         else:
-            self.destiny_membership_id = details['Response'][0]['membershipId']
+            self.destiny_membership_id = details['Response'][i]['membershipId']
 
     def get_last_played(self):
         i = 0
         date_list = []
-        est = timezone('US/Eastern')
         while i < len(self.destiny_account_summary['Response']['data']['characters']):
             date_list.append(self.destiny_account_summary['Response']['data']['characters'][i]['characterBase']['dateLastPlayed'])
             i += 1
-        self.last_played = parse(max(date_list)).astimezone(est)
+        self.last_played = max(date_list)#parse(max(date_list)).astimezone(est)
+        last_played = parser.parse(self.last_played)
+        self.last_played_days = str(self.date_math(last_played))
         
-    def get_destiny_account_summary(self):
-        full_uri = self.destiny_base_uri + self.membership_type + '/Account/' + self.destiny_membership_id + '/Summary/' 
+    def get_destiny_account_summary(self, *DESTINYID):
+        if not DESTINYID:
+            MEMBERSHIPID = self.destiny_membership_id
+        else:
+            MEMBERSHIPID = DESTINYID
+        full_uri = self.destiny_base_uri + self.membership_type + '/Account/' + MEMBERSHIPID + '/Summary/' 
         self.destiny_account_summary = self.get_request(full_uri)
 
     def get_request(self, full_uri):
@@ -76,17 +129,38 @@ class ClanBot(object):
             print "Problem retrieving: {0}".format(full_uri)
             exit(1)
 
-    def initialize(self, gamertag):
+    def initialize(self, *options):
         i = 0
-        while i < len(gamertag):
-            self.gamertag = gamertag[i]
-            self.get_stats()
-            i += 1
+        if options[0].file:
+            with open(options[0].file, 'r') as f:
+                self.clan_dict = json.load(f)
+            f.close()
+        if options[0].update:
+            self.get_clan_members()
+            self.populate_stats()
+        elif options[0].gamertag:
+            while i < len(options[0].gamertag):
+                self.gamertag=options[0].gamertag[i]
+                self.get_stats()
+                i += 1
+
+    def populate_stats(self):
+        for key in self.clan_dict['Members'].keys():
+            self.get_destiny_details(key)
+            self.clan_dict['Members'][key]['destinyMembershipId'] = self.destiny_membership_id
+            self.get_destiny_account_summary()
+            self.get_last_played()
+            self.clan_dict['Members'][key]['lastPlayed'] = self.last_played
+        with open('stats.json', 'w') as f:
+            json.dump(self.clan_dict, f)
+        f.close()
 
     def get_stats(self):
-        #self.get_membership_id()
-        #self.get_destiny_membership_id()
         self.get_destiny_details()
         self.get_destiny_account_summary()
         self.get_last_played()
+        #self.display_stats()
+        #self.get_clan_id()
+        #print json.dumps(self.clan_dict, ensure_ascii=False)
+        #print len(self.clan_dict['Members'])
         self.display_stats()
--- immo.py
+++ immo.py
@@ -4,11 +4,27 @@ import sys
 
 def main():
     parser = argparse.ArgumentParser(prog=sys.argv[0])
+    parser.add_argument('-f', '--file',
+                        action='store',
+                        type=str,
+                        dest='file',
+                        help='stats file to parse')
+    parser.add_argument('-l', '--last-played',
+                        action='store_true',
+                        dest='last_played',
+                        help='display last played time for player')
+    parser.add_argument('-u', '--update',
+                        action='store_true',
+                        dest='update',
+                        help='update stats.json with fresh data, will take a while')
     parser.add_argument('gamertag',
                         nargs=argparse.REMAINDER,
-                        help='Your subject\'s gamertag')
+                        help='your subject\'s gamertag')
     options = parser.parse_args()
-    ClanBot().initialize(options.gamertag)
+    if not options:
+        ClanBot().initialize()
+    else:
+        ClanBot().initialize(options)
 
 if __name__ == "__main__":
     main()
